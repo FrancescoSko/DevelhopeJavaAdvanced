cd25943267fad8ef360b8a8de6ee02eafc00ac23
FrancescoSko <133954449+FrancescoSko@users.noreply.github.com>
2023-07-18 13:13:07 +0200

    Update Main.java
    Modifiche

--- AbstractClasses/Main.java
+++ AbstractClasses/Main.java
@@ -4,11 +4,11 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) {
-     Car car = new Car(5, 40000);
+     Car car = new Car(5, 40000, "Car", 4);
         car.doVehicleSound();
         car.showVehicleDetails();
 
-     Boat boat = new Boat(125, 8500);
+     Boat boat = new Boat(125, 8500, "Boat");
       boat.doVehicleSound();
       boat.showVehicleDetails();
       System.out.println(boat.getBoatWeightAndSpeed());
